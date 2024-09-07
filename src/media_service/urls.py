38e9bc5fe9c1a50fb834a736b750b9d91037f709
daniel <ntdu198@gmail.com>
2024-09-07 10:43:08 +0700

    feat: add sync endpoint

--- src/media_service/urls.py
+++ src/media_service/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/', include("src.core.urls")),
+    path('api/', include("src.api.urls")),
     path('admin/', admin.site.urls),
 ]
